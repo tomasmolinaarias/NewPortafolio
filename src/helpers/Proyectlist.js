65931c2964b8a08bb0c75088e87bc351e793cc11
victor molina <tomasmolinaarias12@gmail.com>
2022-09-17 21:01:34 -0300

    avances

--- src/helpers/Proyectlist.js
+++ src/helpers/Proyectlist.js
@@ -2,14 +2,11 @@ import viajechile from "../assets/img/Proyectos/ViajesChile.png";
 import clubDetiro from "../assets/img/Proyectos/clubdetiros.png";
 import CrudSimple from "../assets/img/Proyectos/curdSimple.png";
 import RickAndMorty from "../assets/img/Proyectos/rickandMorty.png";
+import sugerencias from "../assets/img/Proyectos/Sugerencias.png";
+import meetAndCoffee from "../assets/img/Proyectos/meetAndCoffe.png";
+import suricata from "../assets/img/Proyectos/suricata.png";
+import iguana from "../assets/img/Proyectos/iguana.png";
 export const ProjectList = [
-  {
-    name: "Viajes Chile",
-    image: viajechile,
-    skills: "JavaScript, HTML, CSS y Boostrap",
-    github: "https://github.com/tomasmolinaarias/viajes-chile",
-    url: "https://chile-viajes1-victort-molina.netlify.app",
-  },
   {
     name: "Club de tiro",
     image: clubDetiro,
@@ -30,5 +27,40 @@ export const ProjectList = [
     skills: "HTML ,CSS, React, Bootstrap",
     github: "https://github.com/tomasmolinaarias/RickAndMorty",
     url: "https://rick-and-mortyn.netlify.app",
+  },
+  {
+    name: "Sugerencias",
+    image: sugerencias,
+    skills: "JavaScript, HTML, CSS y Boostrap",
+    github: "https://github.com/tomasmolinaarias/sugerencias",
+    url: "https://tomasmolinaarias.github.io/sugerencias/",
+  },
+  {
+    name: "Viajes Chile",
+    image: viajechile,
+    skills: "JavaScript, HTML, CSS y Boostrap",
+    github: "https://github.com/tomasmolinaarias/viajes-chile",
+    url: "https://chile-viajes1-victort-molina.netlify.app",
+  },
+  {
+    name: "Suricata",
+    image: suricata,
+    skills: "HTML y CSS ",
+    github: "https://github.com/tomasmolinaarias/suricata.git",
+    url: "https://tomasmolinaarias.github.io/suricata/",
+  },
+  {
+    name: "Meet And Coffee",
+    image: meetAndCoffee,
+    skills: "HTML y CSS ",
+    github: "https://github.com/tomasmolinaarias/meet-y-coffee",
+    url: "https://tomasmolinaarias.github.io/meet-y-coffee/",
+  },
+  {
+    name: "Iguana",
+    image: iguana,
+    skills: "HTML y CSS ",
+    github: "https://github.com/tomasmolinaarias/IGUANA",
+    url: "https://tomasmolinaarias.github.io/IGUANA/",
   },
 ];
